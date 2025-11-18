263a99b7e3939231c0491baafcc518153dc6ab74
victorpercio <victorperciodeoliveira@gmail.com>
2025-11-17 21:31:58 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,19 @@
 
+
+<?php
+
+$servidor = "localhost";
+$usuario = "root";      
+$senha = "vitao0206";            
+$banco = "projeto1";    
+
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+if ($conexao->connect_errno) {
+    echo "Erro ao conectar: (" . $conexao->connect_errno . ") " . $conexao->connect_error;
+    exit;
+}
+
+?>
+
+?>
